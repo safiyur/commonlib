78e0dd473a9b481d2711b6894e1fdd97d4767cab
safiyur <safiyurrahman@ymail.com>
2020-11-01 20:57:28 +0530

    Update main.groovy

--- vars/main.groovy
+++ vars/main.groovy
@@ -1,12 +1,4 @@
 def call ()
 {
-    //new appBuild()
-    node
-    {
-    stage("Compile") {
-        steps {
-            bat "./gradlew compileJava"
-        }
-    }
-    }
+    new appBuild()
 }
