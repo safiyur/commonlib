df67b066ea5982a7bb7b9da4e6548fdaa16bc66c
safiyur <safiyurrahman@ymail.com>
2020-11-04 23:19:02 +0530

    Update buildHelper.groovy

--- vars/buildHelper.groovy
+++ vars/buildHelper.groovy
@@ -2,56 +2,43 @@
 
 def compile() {
     stage("compile") {
-        steps
-            {
-                echo "Compiling the app"
-                bat "./gradlew compileJava"
-            }
+        echo "Compiling the app"
+        bat "./gradlew compileJava"
     }
 }
 
 def doPackage() {
     stage("Package") {
-        steps {
-            echo "Packaging starts"
-            bat "./gradlew build"
-        }
+        echo "Packaging starts"
+        bat "./gradlew build"
     }
 }
 
 def doPush() {
     stage("Docker Push") {
-        steps {
-            echo "Pushing to Docker Hub"
-            bat "docker login -u safik1 -p Safrah@165"
-            bat "docker push safik1/calculator"
-        }
+        echo "Pushing to Docker Hub"
+        bat "docker login -u safik1 -p Safrah@165"
+        bat "docker push safik1/calculator"
     }
 }
 
 def buildImage() {
     stage("Docker build") {
-        steps {
-            echo "Build Docker Image"
-            bat "docker build -t safik1/calculator ."
-        }
+        echo "Build Docker Image"
+        bat "docker build -t safik1/calculator ."
     }
 }
 
 def deployToStage() {
     stage("Deploy to Staging") {
-        steps {
-            echo "Deploy to Staging"
-            bat "docker run -d --rm -p 8765:8080 --name calculator safik1/calculator"
-        }
+        echo "Deploy to Staging"
+        bat "docker run -d --rm -p 8765:8080 --name calculator safik1/calculator"
     }
 }
 
 def cleanup() {
     stage("cleanup") {
-        steps {
-            echo "docker cleanup"
-            bat "docker stop calculator"
-        }
+        echo "docker cleanup"
+        bat "docker stop calculator"
     }
 }
