5c8efa6aec149cc2809b00c6ba3bc34cabb22c89
safiyur <safiyurrahman@ymail.com>
2020-11-10 14:09:41 +0530

    Update appBuild.groovy

--- vars/appBuild.groovy
+++ vars/appBuild.groovy
@@ -3,6 +3,9 @@
 def build() {
     node
             {
+                parameters {
+                    string(defaultValue: "123", description: 'This is a parameter', name: 'PARAMETER01')
+                }
                /* buildHelper.compile()
                 buildHelper.doPackage()
                 buildHelper.buildImage()
@@ -11,7 +14,7 @@ def build() {
                 buildHelper.cleanup()*/
 
                 //build (job: 'check')
-                build (job: 'check', parameters: [string(name: 'HELLO', value: 'hello param')])
+                build (job: 'check', parameters: [string(name: 'HELLO', value: String.valueOf(PARAMETER01))])
 
             }
 }
